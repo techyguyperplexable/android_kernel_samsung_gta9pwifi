62a42642bafeab123094c1699c591b4f3fd07f40
techyguyperplexable <objecting@objecting.org>
2025-12-24 14:26:10 +0000

    anykernel: Force copy DTB to split image directory for repack
    Change-Id: I8809a67cc14602b6093f23a37212467e2a9b25be

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -38,21 +38,11 @@ PATCH_VBMETA_FLAG=auto;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-# init.rc
-# backup_file init.rc;
-# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-# backup_file init.tuna.rc;
-# insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-# append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-# backup_file fstab.tuna;
-# patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-# patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-# patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-# append_file fstab.tuna "usbdisk" fstab;
+# Force copy DTB to split image directory for repack
+if [ -f "$AKHOME/dtb" ]; then
+  cp -f "$AKHOME/dtb" "$SPLITIMG/dtb";
+  ui_print "DTB copied to split image directory";
+fi;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
